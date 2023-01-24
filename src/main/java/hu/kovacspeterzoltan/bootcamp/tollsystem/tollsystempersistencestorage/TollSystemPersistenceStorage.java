b711cd25b242c079f0df135b75c0c94e62dc11ed
webkovacs <webkovacs@gmail.com>
2023-01-24 09:58:13 +0100

    ui megjelenítés fix

--- src/main/java/hu/kovacspeterzoltan/bootcamp/tollsystem/tollsystempersistencestorage/TollSystemPersistenceStorage.java
+++ src/main/java/hu/kovacspeterzoltan/bootcamp/tollsystem/tollsystempersistencestorage/TollSystemPersistenceStorage.java
@@ -10,12 +10,13 @@ import java.util.List;
 public class TollSystemPersistenceStorage implements MotorwayVignetteStorageInterface {
     @Override
     public List<MotorwayVignetteEntity> findByRegistrationNumber(String registrationNumber) {
-        MotorwayVignetteEntity lastMV = getLastMotorwayVignetteEntity();
-        MotorwayVignetteEntity currentMV = getCurrentMotorwayVignetteEntity();
-
         List<MotorwayVignetteEntity> motorwayVignettes = new ArrayList<>();
-        motorwayVignettes.add(lastMV);
-        motorwayVignettes.add(currentMV);
+        if ("AA:BB-123".equals(registrationNumber.toUpperCase())) {
+            MotorwayVignetteEntity lastMV = getLastMotorwayVignetteEntity();
+            MotorwayVignetteEntity currentMV = getCurrentMotorwayVignetteEntity();
+            motorwayVignettes.add(lastMV);
+            motorwayVignettes.add(currentMV);
+        }
         return motorwayVignettes;
     }
 
@@ -25,9 +26,9 @@ public class TollSystemPersistenceStorage implements MotorwayVignetteStorageInte
         mv.setVehicleCategory("d1");
         mv.setMotorwayVignetteType("yearly");
         mv.setPrice(10000);
-        mv.setValidFrom(new Date(2021, 1, 1));
-        mv.setValidTo(new Date(2022, 01, 31, 23, 59, 59));
-        mv.setDateOfPurchase(new Date(2021, 1, 10, 10, 25, 38));
+        mv.setValidFrom(new Date(2021-1900, 1, 1));
+        mv.setValidTo(new Date(2022-1900, 01, 31, 23, 59, 59));
+        mv.setDateOfPurchase(new Date(2021-1900, 1, 10, 10, 25, 38));
         return mv;
     }
 
@@ -37,9 +38,9 @@ public class TollSystemPersistenceStorage implements MotorwayVignetteStorageInte
         mv.setVehicleCategory("d1");
         mv.setMotorwayVignetteType("yearly");
         mv.setPrice(15000);
-        mv.setValidFrom(new Date(2023, 1, 1));
-        mv.setValidTo(new Date(2024, 01, 31, 23, 59, 59));
-        mv.setDateOfPurchase(new Date(2023, 1, 12, 15, 30, 52));
+        mv.setValidFrom(new Date(2023-1900, 1, 1));
+        mv.setValidTo(new Date(2024-1900, 01, 31, 23, 59, 59));
+        mv.setDateOfPurchase(new Date(2023-1900, 1, 12, 15, 30, 52));
         return mv;
     }
 }
